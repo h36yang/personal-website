1aedef852fe44c448e57f2bc3e658b44d7d7d3e7
Jack Yang <h36yang@gmail.com>
2020-10-20 19:24:15 -0400

    Created component stubs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { EducationComponent } from './education/education.component';
+import { MoreAboutComponent } from './more-about/more-about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SkillsComponent,
+    ExperiencesComponent,
+    EducationComponent,
+    MoreAboutComponent
   ],
   imports: [
     BrowserModule
