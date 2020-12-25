8a02c8a7bf3f3f680b2b6eb554823ddddc4a2be1
Jack Yang <h36yang@gmail.com>
2020-12-25 02:38:08 -0500

    Updated fontawesome and removed unused Animation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbCollapseModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgxScrollspyModule } from '@uniprank/ngx-scrollspy';
+import { NgwWowModule } from 'ngx-wow';
 import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
 import { NgxPageScrollModule } from 'ngx-page-scroll';
+import { NgxScrollspyModule } from '@uniprank/ngx-scrollspy';
 import { ParallaxModule } from 'ngx-parallax';
-import { NgwWowModule } from 'ngx-wow';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TagCloudModule } from 'angular-tag-cloud-module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SkillsComponent } from './skills/skills.component';
-import { ExperiencesComponent } from './experiences/experiences.component';
-import { EducationComponent } from './education/education.component';
-
 import { EASE_IN_OUT_EXPO } from './easing-logic';
+import { EducationComponent } from './education/education.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SkillsComponent,
+    EducationComponent,
     ExperiencesComponent,
-    EducationComponent
+    FooterComponent,
+    HeaderComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
+    FontAwesomeModule,
     NgbCollapseModule,
     NgbTooltipModule,
-    NgxScrollspyModule.forRoot(),
+    NgwWowModule,
     NgxPageScrollCoreModule.forRoot({ scrollOffset: 50, easingLogic: EASE_IN_OUT_EXPO }),
     NgxPageScrollModule,
-    NgwWowModule,
+    NgxScrollspyModule.forRoot(),
     ParallaxModule,
-    FontAwesomeModule,
     TagCloudModule
   ],
   providers: [],
